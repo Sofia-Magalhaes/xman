f66cb24965e0bac884f1e9f651f2f7d18346f026
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-07-09 12:03:50 -0300

    :ok_hand:  style: refactoring the code for better visualization

--- src/js/index.js
+++ src/js/index.js
@@ -10,27 +10,41 @@ personagens.forEach((personagem) => {
         }
 
         // verificar se já existe um personagem selecionado, se sim, devemos remover a selecao dele
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
+        removerSelecaoDoPersonagem();
 
         personagem.classList.add('selecionado');                        
 
         // Trocar nome, imagem e descrição do personagem
         // pegar elemento do personagem grande pra adicionar as informações nele
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-        
-        // alterar a imagem do personagem grande
-        const idPersonagem = personagem.attributes.id.value;        
-        imagemPersonagemGrande.src = `./src/img/card-${idPersonagem}.png`;
+        alterarImagemPersonagemSelecionado(personagem);
 
         // alterar o nome
-        const nomePersonagem = document.getElementById('nome-personagem');
-        nomePersonagem.innerText = personagem.getAttribute('data-name');
+        alterarNomePersonagemSelecionado(personagem);
 
         // alterar descricao do personagem
-        const descricaoPersonagem = document.getElementById('descricao-personagem');
-        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
-
+        alterarDescricaoPersonagem(personagem);
     })
 })
+
+function alterarDescricaoPersonagem(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    // alterar a imagem do personagem grande
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/img/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
 
